de655c1251492a2fcc406c4f94f76bec46e7a036
Andre Cogo <andrevallerius7@hotmail.com>
2021-01-07 14:14:52 -0300

    Projeto c# ola mundo

---
+++ OlaMundao/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace OlaMundao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(a+b);
+
+        }
+    }
+}
